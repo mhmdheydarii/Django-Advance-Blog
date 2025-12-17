ea3e1301db727b39b21b0267a95d0810705fbf10
mhmdheydarii <heidarimohammadhosin@gmail.com>
2025-12-17 07:35:41 -0800

    some changes on api

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -5,4 +5,5 @@ app_name = 'api'
 
 urlpatterns = [
     path('post/', views.api_post_view, name="post-api"),
+    path('post/<int:id>/', views.api_post_detaile, name="post-detaile")
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,34 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
+from .serializers import PostSerializer
+from blog.models import Post
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
-@api_view()
+@api_view(["GET", "POST"])
 def api_post_view(request):
-    return Response({"name":"mhmd"})
+    if request.method == "GET":
+        posts = Post.objects.filter(status=True)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def api_post_detaile(request, id):
+    post = get_object_or_404(Post,id=id, status=True)
+    if request.method == "GET":
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = PostSerializer(post, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        post.delete()
+        return Response({"detail":"this item temoved succesfully"}, status=status.HTTP_204_NO_CONTENT)
