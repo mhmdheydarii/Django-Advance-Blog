4b46e6143fed535b1c8979faf50619f6aae624fd
mhmdheydarii <heidarimohammadhosin@gmail.com>
2025-12-26 03:13:31 -0800

    login Token

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from accounts.models import User
 from django.core import exceptions
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+
 
 class RegistrationsSerializers(serializers.ModelSerializer):
     password1 = serializers.CharField(max_length=250, write_only=True)
@@ -29,3 +34,44 @@ class RegistrationsSerializers(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop('password1', None)
         return super().create(**validated_data)
+
+
+
+
+
+class CustomAuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(
+        label=_("Email"),
+        write_only=True
+    )
+    password = serializers.CharField(
+        label=_("Password"),
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+    token = serializers.CharField(
+        label=_("Token"),
+        read_only=True
+    )
+
+    def validate(self, attrs):
+        username = attrs.get('email')
+        password = attrs.get('password')
+
+        if username and password:
+            user = authenticate(request=self.context.get('request'),
+                                username=username, password=password)
+
+            # The authenticate call simply returns None for is_active=False
+            # users. (Assuming the default ModelBackend authentication
+            # backend.)
+            if not user:
+                msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = _('Must include "email" and "password".')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
