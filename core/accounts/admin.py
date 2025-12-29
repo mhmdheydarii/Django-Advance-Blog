bf3d8548c8671fc8d82da0767fb9be83705f625e
mhmdheydarii <heidarimohammadhosin@gmail.com>
2025-12-29 08:59:45 -0800

    add is_verified in models

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -5,13 +5,13 @@ from .models import User, Profile
 
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('email', 'is_staff', 'is_active')
+    list_display = ('email', 'is_staff', 'is_active', 'is_verified')
     ordering = ('created_date',)
     search_fields = ('email',)
 
     fieldsets = (
         ("Authentication", {"fields": ("email", "password")}),
-        ("Permissions", {"fields": ("is_staff","is_superuser" ,"is_active")}),
+        ("Permissions", {"fields": ("is_staff","is_superuser" ,"is_active", "is_verified")}),
         ("Groupe Permissions", {"fields": ("groups", "user_permissions")}),
         ("Important Date", {"fields": ("last_login",)})
     )
@@ -21,7 +21,7 @@ class CustomUserAdmin(UserAdmin):
             "classes": ("wide",),
             "fields": (
                 "email", "password1", "password2", "is_staff",
-                "is_active"
+                "is_active", 'is_verified'
             )}
         ),
     )
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -28,6 +28,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_staff",True)
         extra_fields.setdefault("is_superuser",True)
         extra_fields.setdefault("is_active",True)
+        extra_fields.setdefault("is_verified",True)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError(_("Superuser must have is_staff=True."))
@@ -40,7 +41,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=250, unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    # is_verified = models.Booleanfield(default=True) 
+    is_verified = models.BooleanField(default=True) 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS=[]
 
