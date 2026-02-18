0c4874cef40fd387679ee793a58370cea9ec314d
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-02-18 01:38:50 -0800

    add cahce

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("api/v1/", include("accounts.api.v1.urls")),
     path("Email/", views.send_email, name="send_email"),
+    path("test/", views.testCache, name="cache"),
     # path('api/v2/', include('djoser.urls')),
     # path('api/v2/', include('djoser.urls.jwt'))
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .tasks import send_email
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import requests
 # Create your views here.
 
 def send_email(request):
     send_email.delay()
     return HttpResponse("<h1> Email is being sent </h1>")
+
+
+def test(request):
+    response = requests.get()
+    return JsonResponse(response)
